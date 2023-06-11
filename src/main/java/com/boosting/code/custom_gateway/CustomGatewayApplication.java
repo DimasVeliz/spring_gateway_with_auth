d3ef0771c04b3b799caf7ae8feafc051b7ef5ab3
DimasVeliz <trevor90094@yandex.com>
2023-06-11 18:14:04 +0200

    first commit

---
+++ src/main/java/com/boosting/code/custom_gateway/CustomGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.boosting.code.custom_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomGatewayApplication.class, args);
+	}
+
+}
